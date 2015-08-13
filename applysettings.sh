5dc16f480ac791a612594e5cd8aa4162528f8f5c
TJ Colvin <zentraed@hotmail.com>
2015-08-13 18:18:19 -0500

    Replace additional parameters and log files created by applysettings.sh

--- applysettings.sh
+++ applysettings.sh
@@ -1,12 +1,56 @@
 #!/bin/bash
+# Applies parameters from settings.rc to files located in
+# dockerbuilds/{container}/skel/* and places them in 
+# dockerbuilds/{container}/
+
+# Load parameters and helper functions
 source settings.rc
 
 interpolate_file ./dockerbuilds/shrine/skel/Dockerfile "SHRINE_VERSION" "${SHRINE_VERSION}" > dockerbuilds/shrine/Dockerfile
 
-for p in ./dockerbuilds/i2b2*/skel/*properties; do
+for p in dockerbuilds/i2b2*/skel/*properties; do
         skel="skel/"
         outfile=${p/skel/""}
         interpolate_file "${p}" "SHRINE_IP" "${SHRINE_IP}" > "${outfile}"
+done
+
+interpolate_file dockerbuilds/i2b2/skel/CRCLoaderApplicationContext.xml "I2B2_DEFAULT_DATABASE_ADDR" "$I2B2_DEFAULT_DATABASE_ADDR" | \
+    interpolate "I2B2_DB_NAME" "$I2B2_DB_NAME" | \
+    interpolate "I2B2_DB_HIVE_USER" "$I2B2_DB_HIVE_USER" | \
+    interpolate "I2B2_DB_HIVE_PASSWORD" "$I2B2_DB_HIVE_PASSWORD" > dockerbuilds/i2b2/CRCLoaderApplicationContext.xml 
+
+for p in dockerbuilds/i2b2*/skel/*ds-xml; do
+        skel="skel/"
+        outfile=${p/skel/""}
+        interpolate_file "${p}" "I2B2_DB_HIVE_DATASOURCE_NAME" "$I2B2_DB_HIVE_DATASOURCE_NAME" | \
+        interpolate "I2B2_DB_HIVE_JDBC_URL" "$I2B2_DB_HIVE_JDBC_URL" | \
+        interpolate "I2B2_DB_HIVE_USER" "$I2B2_DB_HIVE_USER" | \
+        interpolate "I2B2_DB_HIVE_PASSWORD" "$I2B2_DB_HIVE_PASSWORD" | \
+        interpolate "I2B2_DB_ONT_DATASOURCE_NAME" "$I2B2_DB_ONT_DATASOURCE_NAME" | \
+        interpolate "I2B2_DB_ONT_JDBC_URL" "$I2B2_DB_ONT_JDBC_URL" | \
+        interpolate "I2B2_DB_ONT_USER" "$I2B2_DB_ONT_USER" | \
+        interpolate "I2B2_DB_HIVE_PASSWORD" "$I2B2_DB_HIVE_PASSWORD" | \
+        interpolate "I2B2_DB_PM_USER" "$I2B2_DB_PM_USER" | \
+        interpolate "I2B2_DB_PM_PASSWORD" "$I2B2_DB_PM_PASSWORD" | \
+        interpolate "I2B2_DB_WORK_USER" "$I2B2_DB_WORK_USER" | \
+        interpolate "I2B2_DB_WORK_PASSWORD" "$I2B2_DB_WORK_PASSWORD" | \
+        interpolate "I2B2_DEFAULT_SCHEMA_PASSWORD" "$I2B2_DEFAULT_SCHEMA_PASSWORD" | \
+        interpolate "I2B2_DEFAULT_DATABASE_ADDR" "$I2B2_DEFAULT_DATABASE_ADDR" | \
+        interpolate "I2B2_DB_NAME" "$I2B2_DB_NAME" | \
+        interpolate "I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" "$I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" | \
+        interpolate "I2B2_DB_SHRINE_ONT_JDBC_URL" "$I2B2_DB_SHRINE_ONT_JDBC_URL" | \
+        interpolate "I2B2_DB_SHRINE_ONT_USER" "$I2B2_DB_SHRINE_ONT_USER" | \
+        interpolate "I2B2_DB_SHRINE_ONT_PASSWORD" "$I2B2_DB_SHRINE_ONT_PASSWORD" > "${outfile}"
+done
+
+for p in dockerbuilds/shrine*/skel/*.js; do
+        skel="skel/"
+        outfile=${p/skel/""}
+        interpolate_file "${p}" "SHRINE_IP" "$SHRINE_IP" | \
+        interpolate "SHRINE_SSL_PORT" "$SHRINE_SSL_PORT" | \
+        interpolate "I2B2_REST_IP" "$I2B2_REST_IP" | \
+        interpolate "I2B2_REST_PORT" "$I2B2_REST_PORT" | \
+        interpolate "I2B2_DOMAIN_ID" "$I2B2_DOMAIN_ID" > "${outfile}"
 done
 
 interpolate_file ./dockerbuilds/postgres/skel/i2b2setup.sql "SHRINE_IP" "${SHRINE_IP}" | \
@@ -33,21 +77,6 @@ interpolate "I2B2_DB_ONT_USER" "${I2B2_DB_ONT_USER}" | \
 interpolate "I2B2_DB_SHRINE_ONT_USER" "$I2B2_DB_SHRINE_ONT_USER" | \
 interpolate "I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" "$I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" | \
 interpolate "I2B2_DB_SHRINE_ONT_PASSWORD" "${I2B2_DB_SHRINE_ONT_PASSWORD}" > dockerbuilds/postgres/shrinesetup.sql
-
-interpolate_file dockerbuilds/i2b2/skel/ont-ds.xml "I2B2_DB_HIVE_DATASOURCE_NAME" "$I2B2_DB_HIVE_DATASOURCE_NAME" | \
-interpolate "I2B2_DB_HIVE_JDBC_URL" "$I2B2_DB_HIVE_JDBC_URL" | \
-interpolate "I2B2_DB_HIVE_USER" "$I2B2_DB_HIVE_USER" | \
-interpolate "I2B2_DB_HIVE_PASSWORD" "$I2B2_DB_HIVE_PASSWORD" | \
-interpolate "I2B2_DB_ONT_DATASOURCE_NAME" "$I2B2_DB_ONT_DATASOURCE_NAME" | \
-interpolate "I2B2_DB_ONT_JDBC_URL" "$I2B2_DB_ONT_JDBC_URL" | \
-interpolate "I2B2_DB_ONT_USER" "$I2B2_DB_ONT_USER" | \
-interpolate "I2B2_DB_HIVE_PASSWORD" "$I2B2_DB_HIVE_PASSWORD" | \
-interpolate "I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" "$I2B2_DB_SHRINE_ONT_DATASOURCE_NAME" | \
-interpolate "I2B2_DB_SHRINE_ONT_JDBC_URL" "$I2B2_DB_SHRINE_ONT_JDBC_URL" | \
-interpolate "I2B2_DB_SHRINE_ONT_USER" "$I2B2_DB_SHRINE_ONT_USER" | \
-interpolate "I2B2_DB_SHRINE_ONT_PASSWORD" "$I2B2_DB_SHRINE_ONT_PASSWORD" > dockerbuilds/i2b2/ont-ds.xml
-
-
 
 
 
