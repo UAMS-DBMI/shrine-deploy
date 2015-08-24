d51f2c243063606aab9323df3144b497f0afe045
Terra Colvin Jr <tdcolvin@uams.edu>
2015-08-24 16:09:45 -0500

    Added file ids for i2b2 versions 1.7.05 and 1.7.06

--- dockerbuilds/i2b2admin/downloadi2b2.sh
+++ dockerbuilds/i2b2admin/downloadi2b2.sh
@@ -5,9 +5,24 @@
 # Usage: downloadi2b2.sh file_id filename_to_save
 #
 # Check official i2b2 website for desired file_id
+#
+# Do not use this script to download files if you have not accepted the i2b2 
+# Software License
+#
+# Examples:
+#
+# id	file
+#
+# 390	i2b2core-src-1706.zip
+# 389	i2b2webclient-1706.zip
+# 387	i2b2createdb-1706.zip
+#
+# 375	i2b2core-src-1705.zip
+# 377	i2b2webclient-1705.zip
+# 378	i2b2createdb-1705.zip
 
 curl --silent -c cookies.txt "https://www.i2b2.org/software/download.html?d=$1&s=2" > /dev/null
 curl --silent -b cookies.txt "https://www.i2b2.org/software/download.html?d=$1&s=2" > /dev/null
 curl --silent -b cookies.txt -d "d=$1&sp=3&submit=I+ACCEPT" "https://www.i2b2.org/software/download.html" > /dev/null
-curl -o $2 -J -b cookies.txt -d "d=$1&sp=4" "https://www.i2b2.org/software/download.html"
+curl -o "$2" -J -b cookies.txt -d "d=$1&sp=4" "https://www.i2b2.org/software/download.html"
 rm cookies.txt
